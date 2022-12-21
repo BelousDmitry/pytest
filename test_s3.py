dea7fb8005aca9e159e4788ed9ca12b312aa276a
BelousDmitry <58919860+BelousDmitry@users.noreply.github.com>
2022-12-21 16:35:46 -0500

    Update test_s3.py

--- test_s3.py
+++ test_s3.py
@@ -8,7 +8,7 @@ from moto import mock_s3
 TEST_BUCKET = "test"
 TEST_FILE = "bilous.moto.txt"
 ENDPOINT_URL = 'http://127.0.0.1:4566'
-AWS_PROFILE = 'default'
+#AWS_PROFILE = 'default'
 AWS_REGION = 'us-east-1'
 
 @mock_s3
@@ -17,7 +17,8 @@ def test_upload():
     global TEST_BUCKET
     global TEST_FILE
     # With the moto library imported, the boto3 s3 is fake
-    boto3.setup_default_session(profile_name=AWS_PROFILE)
+    #boto3.setup_default_session(profile_name=AWS_PROFILE)
+    boto3.setup_default_session()
     s3_client = boto3.client("s3", region_name=AWS_REGION, endpoint_url=ENDPOINT_URL)
     # We need to create the bucket since this is all in Moto's 'virtual' AWS account
     s3_client.create_bucket(Bucket=TEST_BUCKET)
